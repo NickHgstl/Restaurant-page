ede537e6d11c36f0cdf3a75906844a3409184ac6
NickHgstl <nickhoogst@gmail.com>
2023-03-29 16:53:37 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.css';
+
+console.log('hi');
+let div = document.querySelector('#content');
+let button = document.createElement("button");
+div.append(button);
+button.innerHTML = "hi";
